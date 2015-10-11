52782580767e1ca33588bdd2accb78e957b80160
vagrant <vagrant@localhost.localdomain>
2015-10-11 05:50:49 +0000

    不要となったimport文を削除

--- src/main/java/yukimura/sample/dao/Dao.java
+++ src/main/java/yukimura/sample/dao/Dao.java
@@ -3,7 +3,6 @@ package yukimura.sample.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.HashMap;
